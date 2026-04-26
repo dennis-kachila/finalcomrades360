d7d7838bf237c015c444d1558abf4b795a67c660
Dunstun <wambutsidunstun@gmail.com>
2026-04-26 16:11:07 +0300

    fix: make wallet-type migration idempotent

--- backend/migrations/20260410000001-add-wallet-type-to-transaction.js
+++ backend/migrations/20260410000001-add-wallet-type-to-transaction.js
@@ -2,12 +2,15 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Transaction', 'walletType', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      defaultValue: null,
-      comment: 'Which wallet this transaction belongs to: seller, delivery_agent, customer, marketer, service_provider'
-    });
+    const tableInfo = await queryInterface.describeTable('Transaction');
+    if (!tableInfo.walletType) {
+      await queryInterface.addColumn('Transaction', 'walletType', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+        comment: 'Which wallet this transaction belongs to: seller, delivery_agent, customer, marketer, service_provider'
+      });
+    }
   },
 
   async down(queryInterface) {
